07fa2f529fdc5b5eb4152a542482b6624eb50b0b
Camron <camron@metafour.org>
2013-05-02 20:50:49 -0600

    Add names.txt for Problem 22. Update Problem 22

--- euler22.rb
+++ euler22.rb
@@ -1,11 +1,6 @@
 #!/usr/bin/env ruby
 
-$letterValues = [1, "a", 2, "b", 3, "c", 4, "d", 5, "e", 6, "f", 7, "g", 8, "h", 9, "i", 10, "j", 11, "k", 12, "l", 13, "m", 14, "n", 15, "o", 16, "p", 17, "q", 18, "r", 19, "s", 20, "t", 21, "u", 22, "v", 23, "w", 24, "x", 25, "y", 26, "z"]
-
-$letterValues = Hash[*$letterValues]
-
 names_input = File.open(ARGV[0])
-#names_output = File.open(ARGV[0] + "alpha", 'w')
 
 def createArray(filename)
   filename.each { |line|
@@ -61,9 +56,6 @@ letterValues = {
 
     line += 1
 
-#    if name[0] == "a"
-#      puts "#{line}: #{name} #{wordvalue}"
-#    end
   }
   puts sum
 end
